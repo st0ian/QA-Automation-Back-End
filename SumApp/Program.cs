748d4749dc49371fd64793d94cbc63bf57006eeb
Stoyangit config --global user.email stoian2010@abv.bggit config --global core.autocrlf truegit config --global user.name Stoyan <stoian2010@abv.bg>
2023-01-22 21:31:26 +0200

    Add Sum()

--- SumApp/Program.cs
+++ SumApp/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int Sum(int[] arr)
+            {
+                int sum = arr[0];
+                for (int i = 1; i < arr
+                  .Length; i++)
+                    sum += arr[i];
+                return sum;
+            }
         }
     }
 }
