edc5adfc3803f304d66585cd4b5276a94fe33007
Stoyangit config --global user.email stoian2010@abv.bggit config --global core.autocrlf truegit config --global user.name Stoyan <stoian2010@abv.bg>
2023-01-28 18:35:09 +0200

    new changes

---
+++ Collections_UnitTesting_Part2/Collections.UnitTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace Collections.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            // neq change
+            Assert.Pass();
+        }
+    }
+}
