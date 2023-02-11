2beb7b27e0ef9b09c4c9556957f08223fc7fbf66
Stoyangit config --global user.email stoian2010@abv.bggit config --global core.autocrlf truegit config --global user.name Stoyan <stoian2010@abv.bg>
2023-02-11 10:12:29 +0200

    update file structure

---
+++ RestSharp/RestSharpDemoProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using RestSharp;
+
+namespace RestSharpDemoProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RestClient client = new RestClient("https://api.github.com");
+            RestRequest request = new RestRequest("repos/{user}/{repoName}/issues/1", Method.Get);
+
+            request.AddUrlSegment("user","");
+            request.AddUrlSegment("repoName","");
+        }
+    }
+}
